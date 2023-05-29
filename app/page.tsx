b04cb54f432ebc5d3d652d150a679c8f6f12cdd0
Justin Sixsmith <justinsixsmith@gmail.com>
2023-05-29 08:28:25 -0500

    Adds animation and snaps to center

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      {/* Header */}
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
+
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
       {/* Experience */}
 
---
+++ components/Hero.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import pic from "@/public/pic.jpg";
+import Image from "next/image";
+
+type Props = {};
+
+function Hero(props: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Justin Sixsmith.",
+      "Guy-who-loves-Board-Games.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={pic}
+        alt=""
+      />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
