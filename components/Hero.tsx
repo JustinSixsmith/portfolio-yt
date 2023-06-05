8effa956ce3b5234943f89f96d9781ecdc67f4b4
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-05 12:29:24 -0500

    Resizes hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ function Hero({ pageInfo }: Props) {
   });
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <div className="h-screen flex flex-col space-y-10 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
       <img
         src={urlFor(pageInfo?.heroImage).url()}
@@ -30,10 +30,10 @@ function Hero({ pageInfo }: Props) {
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
       />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+        <h2 className="text-xs md:text-base lg:text-xl uppercase text-gray-500 pb-2 tracking-[6px] md:tracking-[12px] lg:tracking-[18px]">
           {pageInfo?.role}
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-lg md:text-3xl lg:text-5xl font-semibold px-5">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
