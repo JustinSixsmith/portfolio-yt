322c39b23106ec01da24a9f402b35f2525b28db4
Justin Sixsmith <justinsixsmith@gmail.com>
2023-07-21 01:19:05 -0500

    Fixes some margin/padding issues in hero

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,11 @@ export default function Home() {
     <main className="main-div bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 smooth-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header socials={socials} />
 
-      <section id="hero" className="snap-start">
+      <section
+        id="hero"
+        className="snap-start pt-10"
+        style={{ marginTop: '-100px' }}
+      >
         {pageInfo && <Hero pageInfo={pageInfo} />}
       </section>
 
